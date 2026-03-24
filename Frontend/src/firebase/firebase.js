97528970053357cce6b84c0ace01782c87445cb8
poppy-may <poppy.may.johnson@gmail.com>
2026-03-24 21:37:20 +0000

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- Frontend/src/firebase/firebase.js
+++ Frontend/src/firebase/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyrHx0PscJ_6NvLIsChDl0mVjxWtcgtY8",
-  authDomain: "project1-4ffa4.firebaseapp.com",
-  projectId: "project1-4ffa4",
-  storageBucket: "project1-4ffa4.firebasestorage.app",
-  messagingSenderId: "834369789957",
-  appId: "1:834369789957:web:fd0c6eb09d13f2a3537c6c",
-  measurementId: "G-SSXZQ1MEVW"
+  
 };
 
 const app = initializeApp(firebaseConfig);
